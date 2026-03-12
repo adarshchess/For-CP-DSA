fa81b10d09388e4de4848ee4099475e6700126d9
CottonMouth <itsadarsh49@gmail.com>
2026-03-13 01:36:57 +0530

    adarsh_soln

---
+++ CodeChefContests/Starters229/all_zeros.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t=0;
+	cin>>t;
+	while(t--){
+	    int a=0;
+	    int b=0;
+	    int c=0;
+	    cin>>a>>b>>c;
+	    //make eqn to see the vals of variables as compared to final val of a,b,c which is equal to zero
+	    if(c%3==0&&b==(2*a+c/3)){
+	        
+	            cout<<"YES"<<endl;
+	        
+	        
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	    
+	}
+
+}
